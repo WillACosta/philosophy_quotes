e51831ff043a54226c8fdef9afb28164f4370699
willACosta <williamamaral521@gmail.com>
2022-10-09 10:57:56 -0300

    refactor(my-quotes): add layout for no quotes

--- app/src/main/java/com/example/philosophyquotes/view/fragments/MyQuotesFragment.kt
+++ app/src/main/java/com/example/philosophyquotes/view/fragments/MyQuotesFragment.kt
@@ -5,15 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.philosophyquotes.R
+import com.example.philosophyquotes.databinding.FragmentMyQuotesBinding
 
 class MyQuotesFragment : Fragment() {
+    private var _binding: FragmentMyQuotesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentMyQuotesBinding.inflate(inflater, container, false)
 
-        return inflater.inflate(R.layout.fragment_my_quotes, container, false)
+        return binding.root
     }
 }
